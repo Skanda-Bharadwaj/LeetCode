3ff6697be854323055c9bbed9ad9bc0fac54e16a
Skanda Bharadwaj <skandabharadwaj94@gmail.com>
2020-08-05 18:05:16 -0400

    4Sum

--- 4Sum/4Sum/main.cpp
+++ 4Sum/4Sum/main.cpp
@@ -18,7 +18,7 @@ int main() {
     nums.push_back(-1);
     nums.push_back(0);
     nums.push_back(-2);
-    nums.push_back(2);
+//    nums.push_back(2);
     
     int target = 0;
     vector<vector<int>> res = s.fourSum(nums, target);
